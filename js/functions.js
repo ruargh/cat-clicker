9fdc1681c72ee5afe56996493f5eafa08b9623ba
Ruargh <ruargh@gmail.com>
2018-05-11 23:42:06 +0200

    l1-13a

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,20 @@
-var clicks = 0
-var clicks2 = 0
+const cats = [
+  { name: 'Scrappy', clicks: '1', img: '/img/cat1.jpg' },
+  { name: 'Mr Mistoffelees', clicks: '2', img: '/img/cat2.jpg' },
+  { name: 'Garfield', clicks: '3', img: '/img/cat3.jpg' },
+  { name: 'Felix', clicks: '4', img: '/img/cat4.jpg' },
+  { name: 'Sylvetor', clicks: '5', img: '/img/cat5.jpg' },
+];
 
-var counter = document.getElementById('counter')
-var counter2 = document.getElementById('counter2')
-
-function addClick() {
-    clicks++
-    counter.innerHTML = clicks
+const counter = document.getElementById('counter');
+function addClick(index) {
+  cats[index].clicks += 1;
+  counter.innerHTML = cats[index].clicks;
 }
 
-function addClick2() {
-    clicks2++
-    counter2.innerHTML = clicks2
-}
-
-var el = document.getElementById('cat-pic')
-el.addEventListener('click', addClick, false)
-
-var el2 = document.getElementById('cat-pic2')
-el2.addEventListener('click', addClick2, false)
+const el = document.getElementsByClassName('catList');
+el[0].addEventListener('click', addClick(0), false);
+el[1].addEventListener('click', addClick(1), false);
+el[2].addEventListener('click', addClick(2), false);
+el[3].addEventListener('click', addClick(3), false);
+el[4].addEventListener('click', addClick(4), false);
